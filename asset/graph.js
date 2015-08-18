16199096f29d23327a53720ebf09081193d41dd5
byjo <buyongjo@gmail.com>
2015-08-18 09:40:28 +0900

    #3 draw svg graph using d3.js

--- asset/graph.js
+++ asset/graph.js
@@ -1,3 +1,5 @@
+
+
 (function() {
 
 	var data = [
@@ -8,53 +10,58 @@
 		{date: 5, temperature: 26}
 	];
 
-	var width = 500;
-	var height = 800;
-	var margin = {top: 30, right: 20, bottom: 30, left: 50};
+	var width = 800;
+	var height = 500;
+	var margin = {top:50, right:100, bottom: 50, left: 100};
 
-	var xRange = d3.scale.linear().range([margin.left, width - margin.right]).domain([d3.min(data, function(d) {
-	      return d.x;
-	    }), d3.max(data, function(d) {
-	      return d.x;
-	    })]);
-	var yRange = d3.scale.linear().range([height - margin.top, margin.bottom]).domain([d3.min(data, function(d) {
-	      return d.y;
-	    }), d3.max(data, function(d) {
-	      return d.y;
-	    })]);
+	var minTemp = d3.min(data, function(d) {
+		return d.temperature;
+	});
+	var maxTemp = d3.max(data, function(d) {
+		return d.temperature;
+	});
 
-	var x = d3.scale.linear().range([0, width]);
-	var y = d3.scale.linear().range([height, 0]);
+	var x = d3.scale.linear().range([margin.left, width-margin.right]).domain([1, 5]);
+	var y = d3.scale.linear().range([height-margin.top, margin.bottom]).domain([minTemp, maxTemp]);
 
-	var xAxis = d3.svg.axis().scale(x).orient("bottom").ticks(5);
-	var yAxis = d3.svg.axis().scale(y).orient("left").ticks(5);
+	var xAxis = d3.svg.axis()
+		.scale(x)
+		.orient("bottom")
+		.ticks(5)
+		.tickSubdivide(true);	
 
-	var valueline = d3.svg.line()
-		.x(function(d) { return x(d.date); })
-		.y(function(d) { return y(d.temperature); });
-
-	var lineFunc = d3.svg.line()
-		.x(function(d) {
-			return xRange(d.x);
-		})
-		.y(function(d) {
-			return yRange(d.y);
-		})
-		.interpolate("linear");
+	var yAxis = d3.svg.axis()
+		.scale(y)
+		.orient("left")
+		.ticks(5)
+		.tickSubdivide(true);
 
 	var svg = d3.select("#d3Graph");
 
 	svg.append("svg:g")
-		.attr("class", "grid")
+		.attr("class", "axis")
+		.attr("transform", "translate(0," + (height - margin.bottom) + ")")
 		.call(xAxis);
 
 	svg.append("svg:g")
-		.attr("class", "grid")
+		.attr("class", "axis")
+		.attr("transform", "translate(" + (margin.left) + ",0)")
 		.call(yAxis);
 
+
+	var lineFunc = d3.svg.line()
+		.x(function(d) {
+			return x(d.date);
+		})
+		.y(function(d) {
+			return y(d.temperature);
+		})
+		.interpolate("linear");
+
 	svg.append("svg:path")
-		.attr("class", "lines")
-		.attr("d", lineFunc(data));
+		.attr("d", lineFunc(data))
+		.attr("class", "lines");
+
 
 }());
 
