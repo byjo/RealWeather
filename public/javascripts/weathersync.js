b5df9b70806870ef50cc17e3bb306eec2cd58685
byjo <buyongjo@gmail.com>
2015-09-22 12:33:05 +0900

    update hosts

--- public/javascripts/weathersync.js
+++ public/javascripts/weathersync.js
@@ -1,7 +1,10 @@
 var RWEATHERsync = {
 	weather : {},
+	HTTPURL : "http://125.209.194.165:3000/weather",
+	SOCKETURL : "ws://125.209.194.165:8080",
+
 	init : function() {
-		var url = "ws://125.209.194.165:8080";
+		var url = this.SOCKETURL;
 		//var url = "ws://localhost:8080";
 		this.connection = new WebSocket(url);
 
@@ -46,7 +49,7 @@ var RWEATHERsync = {
 		formData.append("photo", this.photo);
 
 		var xhr = new XMLHttpRequest;
-		xhr.open('POST', 'http://localhost:3000/weather', true);
+		xhr.open('POST', THIS.HTTPURL, true);
 		xhr.send(formData);
 
 		this.connection.send(JSON.stringify(this.weather));
@@ -70,7 +73,7 @@ var RWEATHERsync = {
 	},
 
 	getWeatherData : function(callback) {
-		var url = "http://125.209.194.165:3000/weather";
+		var url = THIS.HTTPURL;
 		//var url = "http://localhost:3000/weather";
 		$.get(url, this.weather).done(function(response) {
 			// mongodb에서 온 data는 json 이구나?
